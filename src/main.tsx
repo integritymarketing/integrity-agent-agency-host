4cdca891653cefb0e0f1724d1c8e4b0546370abe
samrat <samrat.lekkala@integrity.com>
2025-05-16 11:49:32 -0500

    refactor: remove unnecessary React import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // <-- import BrowserRouter
 // import { AuthProvider } from "./contexts/auth0Provider/AuthContext";
