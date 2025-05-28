fb6654689f375bac60c977a40916b475eedb7a6f
unstephenk <unstephenk@gmail.com>
2025-05-28 10:34:58 -0500

    attached to mfe element ID instead of root

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-// @ts-ignore
-// @ts-ignore
 export default defineConfig({
   resolve: {
     alias: {
