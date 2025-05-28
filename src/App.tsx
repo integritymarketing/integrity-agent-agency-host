fd09d0f56fced3d45bb70d156ff477025153fd13
vishalmarneni <vishal.marneni@integritymarketing.com>
2025-05-28 14:16:44 -0500

    refactor: streamline RouterSwitch component for improved authentication handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./App.css";
 import AppRoutes from "@/routes/AppRoutes";
@@ -6,17 +7,35 @@ import AuthRoutes from "@/routes/AuthRoutes";
 import { AuthProvider } from "@/contexts/auth0Provider/AuthContext";
 import { AgentGlobalProvider } from "@/contexts";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import { CircularProgress, Box } from "@mui/material";
+
+function RouterSwitch() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+        <CircularProgress color="primary" />
+      </Box>
+    );
+  }
+
+  if (isAuthenticated) {
+    return (
+      <AgentGlobalProvider>
+        <AppRoutes />
+      </AgentGlobalProvider>
+    );
+  }
+  return <AuthRoutes />;
+}
 
 function App() {
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <AuthProvider>
-          <AuthRoutes />
-        {/* App (microfrontend) routes wrapped in AgentGlobalProvider */}
-        <AgentGlobalProvider>
-          <AppRoutes />
-        </AgentGlobalProvider>
+          <RouterSwitch />
         </AuthProvider>
       </BrowserRouter>
     </ErrorBoundary>
